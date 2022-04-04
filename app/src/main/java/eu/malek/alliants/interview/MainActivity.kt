73af09c1c5b9027f09007672c8e1035c170f0565
Ondrej Malek <o.malek.cz@gmail.com>
2022-04-04 11:56:33 +0200

    task1: init project from AndroidStudio wizzard fragment+viewmodel

---
+++ app/src/main/java/eu/malek/alliants/interview/MainActivity.kt
@@ -0,0 +1,18 @@
+package eu.malek.alliants.interview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.malek.alliants.interview.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/eu/malek/alliants/interview/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package eu.malek.alliants.interview.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
